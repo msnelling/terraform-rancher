e11836626e75528eeb6a3f15555fc5620f3e0de2
Mark Snelling <mark@dev01.xmple.io>
2019-10-09 14:28:45 +0100

    Updated cluster outputs.

--- 02-test-cluster/outputs.tf
+++ 02-test-cluster/outputs.tf
@@ -1,8 +1,17 @@
 output kube_config {
-  value     = "${rancher2_cluster.cluster.kube_config}"
+  value     = rancher2_cluster.cluster.kube_config
   sensitive = true
 }
 
-output system_project_id {
-  value = rancher2_cluster.cluster.system_project_id
+output cluster_id {
+  value = rancher2_cluster.cluster.id
+}
+
+output k8s_api_endpoint {
+  value = yamldecode(rancher2_cluster.cluster.kube_config).clusters[0].cluster.server
+}
+
+output k8s_api_token {
+  value = yamldecode(rancher2_cluster.cluster.kube_config).users[0].user.token
+  sensitive = true
 }
--- 02-test-cluster/providers.tf
+++ 02-test-cluster/providers.tf
@@ -10,7 +10,6 @@ provider dns {
 provider rancher2 {
   api_url   = data.terraform_remote_state.rancher.outputs.api_url
   token_key = data.terraform_remote_state.rancher.outputs.token_key
-  insecure  = true
 }
 
 provider vsphere {
