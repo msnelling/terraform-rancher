0b1721b2fc38f3697b58e4e23db5ec0547209437
Mark Snelling <mark@bakedbeans.com>
2019-10-27 12:15:07 +0000

    Moved unused stuff to other folder.

---
+++ 99-misc/data.tf
@@ -0,0 +1,48 @@
+data terraform_remote_state rancher {
+  #  backend = "remote"
+  backend = "local"
+  config = {
+    #    organization = "xmple"
+    #    workspaces = {
+    #      name = "rancher"
+    #    }
+    path = "${path.module}/../01-rancher-server/terraform.tfstate"
+  }
+}
+
+data terraform_remote_state cluster {
+  #  backend = "remote"
+  backend = "local"
+  config = {
+    #    organization = "xmple"
+    #    workspaces = {
+    #      name = "k8s"
+    #    }
+    path = "${path.module}/../02-cluster/terraform.tfstate"
+  }
+}
+
+data rancher2_project system {
+  cluster_id = data.terraform_remote_state.cluster.outputs.cluster_id
+  name       = "System"
+}
+
+data template_file cert_manager_staging_issuer {
+  template = file("${path.module}/templates/cloudflare_issuer.yaml.tpl")
+  vars = {
+    issuer_name          = "letsencrypt-staging"
+    acme_ca_server       = "https://acme-staging-v02.api.letsencrypt.org/directory"
+    acme_email           = var.acme_email
+    cloudflare_api_email = var.cloudflare_api_email
+  }
+}
+
+data template_file cert_manager_production_issuer {
+  template = file("${path.module}/templates/cloudflare_issuer.yaml.tpl")
+  vars = {
+    issuer_name          = "letsencrypt-production"
+    acme_ca_server       = "https://acme-v02.api.letsencrypt.org/directory"
+    acme_email           = var.acme_email
+    cloudflare_api_email = var.cloudflare_api_email
+  }
+}
---
+++ 99-misc/main.tf
@@ -0,0 +1,16 @@
+/*terraform {
+  backend "remote" {
+    #hostname     = "app.terraform.io"
+    organization = "xmple"
+    workspaces {
+      name = "k8s-apps"
+    }
+  }
+}*/
+
+
+resource local_file kube_config {
+  sensitive_content = data.terraform_remote_state.cluster.outputs.kube_config
+  filename          = "${path.module}/outputs/kubeconfig"
+  file_permission   = "0600"
+}
